0a8ab643dec61547aa0a6572c5d5a30d37902618
Mert-Ozbolat <mertozbolat008@gmail.com>
2025-01-09 16:28:16 +0300

    add

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from './hero'
+import Statistics from './statistics'
+import Map from './map'
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <Statistics />
+
+            <Map />
         </div>
     )
 }
